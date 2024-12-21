05c62628a8d352b7ed60a68df31532e4a33056f3
Vladislav Rytov <60110864+PartyMar@users.noreply.github.com>
2024-12-21 12:26:29 +0300

    add katana gesture

--- Assets/Scripts/Effects/DissolveSlider.cs
+++ Assets/Scripts/Effects/DissolveSlider.cs
@@ -1,37 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DissolveSlider : MonoBehaviour
 {
-    [Range(0, 1)] public float dissolveValue = 0.7f;
+    [Range(0, 1)] public float dissolveToValue = 1f;
+
+    private float _dissolvePreviousValue;
 
     private bool _activated = true;
     private Material _material;
 
+
+    private float _fraction = 0;
+
     void Start()
     {
         _material = GetComponent<Renderer>().material;
-        _material.SetFloat("_DissolveVal", dissolveValue);
+        _material.SetFloat("_DissolveVal", dissolveToValue);
+
+        _dissolvePreviousValue = dissolveToValue;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (dissolveValue <= 0)
-        {
-            _activated = false;
-            return;
-        }
         if (!_activated) return;
-        
-        _material.SetFloat("_DissolveVal", dissolveValue);
+
+        if (_fraction >= 1) return;
+
+
+        _fraction += Time.deltaTime;
+        if (_fraction >= 1) _fraction = 1;
+        float newValue = Mathf.Lerp(_dissolvePreviousValue, dissolveToValue, _fraction);
+        _material.SetFloat("_DissolveVal", newValue);
     }
 
     public void UpdateDisolveValue(float val)
     {
+        _fraction = 0;
+        _dissolvePreviousValue = dissolveToValue;
         if (val > 1) val = 1;
-        if (val < 0) val = 0;
-        dissolveValue = val;
+        else if (val < 0) val = 0;
+        else dissolveToValue = val;
     }
 
     public void Activate()
