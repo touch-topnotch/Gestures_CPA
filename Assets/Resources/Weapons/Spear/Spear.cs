d97d0c1ab3deba1a9f9df42512eea561b9b80ffe
IlyaOlokin <i.olokin@mail.ru>
2025-04-14 14:26:53 +0500

    vfx

--- Assets/Resources/Weapons/Spear/Spear.cs
+++ Assets/Resources/Weapons/Spear/Spear.cs
@@ -10,9 +10,9 @@ using UnityEngine;
 public class Spear : WeaponDesign
 {
     [Header("Spear Settings")]
-    [SerializeField] private Transform _spawnPoint;
     [SerializeField] private Transform _endSpawnPoint;
     [SerializeField] private float _spawnDuration;
+    [SerializeField] private Animation _spawnAnimation;
     [SerializeField] private GameObject _spearObject;
     [SerializeField] private SpearAura _spearAura;
 
@@ -32,32 +32,31 @@ public class Spear : WeaponDesign
         switch (frameId)
         {
             case 0:
-                transform.position = playerData.playerTransform.position;
+                transform.position = _endSpawnPoint.transform.position;
                 transform.rotation = playerData.playerTransform.rotation;
                 break;
+            case 7:
+                vfxProcessor.ActivateResource("Frame_" + frameId, (GameObject o)=>
+                {
+                    o.SetActive(true);
+                });
+                break;
             case 8:
-                _spearObject.transform.position = _spawnPoint.position;
                 _spearObject.transform.rotation = Quaternion.LookRotation(Vector3.up);
-                _spearObject.SetActive(true);
+                
                 StartCoroutine(SpawnSpear());
                 break;
         }
     }
-    
+
     private IEnumerator SpawnSpear()
     {
-        float startTime = Time.time;
-        float journeyLength = Vector3.Distance(_spawnPoint.position, _endSpawnPoint.position);
+        yield return new WaitForSeconds(1 - _spawnDuration);
+        _spearObject.SetActive(true);
+        _spawnAnimation.Play();
 
-        while (Time.time < startTime + _spawnDuration)
-        {
-            float distCovered = (Time.time - startTime) * journeyLength / _spawnDuration;
-            float fracJourney = distCovered / journeyLength;
-            _spearObject.transform.position = Vector3.Lerp(_spawnPoint.position, _endSpawnPoint.position, fracJourney);
-            yield return null;
-        }
-
-        _spearObject.transform.position = _endSpawnPoint.position;
+        yield return new WaitForSeconds(_spawnDuration);
+        
         _spearAura.gameObject.SetActive(true);
     }
 
