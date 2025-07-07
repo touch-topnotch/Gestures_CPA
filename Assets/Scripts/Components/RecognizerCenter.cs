085ffaaa3af3b2c78c23259fd1e5d19a89c3e80d
Dmitry057 <d.tetkin@innopolis.university>
2025-07-08 02:14:28 +0300

    recognizer center fix

--- Assets/Scripts/Components/RecognizerCenter.cs
+++ Assets/Scripts/Components/RecognizerCenter.cs
@@ -5,16 +5,18 @@ namespace Scripts
     {
         public Vector3 offset;
         public Transform body;
-
+        private Transform root;
         private void Start()
         {
             offset = this.transform.localPosition;
+            root = body.parent;
         }
         // Update is called once per frame
         void Update()
         {
             var transform1 = this.transform;
-            transform1.position = body.TransformPoint(offset);
+            var tr = body.TransformPoint(offset);
+            transform1.position = new Vector3(tr.x, root.position.y, tr.z);
             transform1.rotation = body.rotation;
         }
     }
